668b5db607f96ff68343af551003a68522d7753e
Chely Ho <adept_inept@yahoo.com>
2018-03-16 18:31:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-    erb :index
-  end
-
   get '/recipes' do
     # @recipes = Recipe.all
     erb :index

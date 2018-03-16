3f1fcbb123bc38c2727ffbbc24008e1c4cb7c9a3
Chely Ho <adept_inept@yahoo.com>
2018-03-16 18:09:27 +0000

    automatically backed up by learn

--- db/migrate/20180316180542_create_recipes.rb
+++ db/migrate/20180316180542_create_recipes.rb
@@ -1,4 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration[5.1]
   def change
+    create_table :recipes do |t|
+      t.string :name
+      t.text :ingredients
+      t.string :cook_time
   end
 end
